6fa96ae0b25718c7dce66c69800f17fb7df82427
ryanwalker64 <ryan.walkerz46@gmail.com>
2022-11-24 10:09:49 +1300

    commit

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const { request } = require('express')
 const express = require('express')
+const cors = require('cors')
 const app = express()
+
+app.use(express.json())
+app.use(cors())
 
 let phonebook = [
     { 
